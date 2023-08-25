20181f1c6859e343a956a91ed13f3915cbc53d08
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-08-25 17:25:06 +0300

    Initial Commit

---
+++ UIComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIComponents
+//
+//  Created by Stanislav Lemeshaev on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
