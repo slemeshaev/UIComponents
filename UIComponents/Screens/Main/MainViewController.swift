22a6c7d1a8fbb4a3eea0cdf5fb8ed4b78c26478b
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2023-08-25 17:51:38 +0300

    Initial project

---
+++ UIComponents/Screens/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  UIComponents
+//
+//  Created by Stanislav Lemeshaev on 25.08.2023.
+//  Copyright © 2023 slemeshaev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+}
